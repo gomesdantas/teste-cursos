1281f1c4885682cf7844a7f55654614f68cfcf96
Bruno O <brunodeoliveira.22.10@gmail.com>
2023-09-08 12:45:23 -0300

    ok

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let respostas = ['CV', 'CV', 'Informação', 'Informação', 'CV', 'Administração'];
+//
+//
+//
+//
+
+let cursos = [];
+for (let item of respostas) {
+  let curso = cursos.find(c => c.nome == item);
+    
+}
+
+
